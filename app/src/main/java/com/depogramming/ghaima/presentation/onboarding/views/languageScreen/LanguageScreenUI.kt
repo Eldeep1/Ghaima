56d84cfb1c5984baa5e39533ac93cb3102f5725a
Eldeep1 <alieldeep111@gmail.com>
2026-03-09 07:59:48 +0200

    solving some IDE warnnings

--- app/src/main/java/com/depogramming/ghaima/presentation/onboarding/views/languageScreen/LanguageScreenUI.kt
+++ app/src/main/java/com/depogramming/ghaima/presentation/onboarding/views/languageScreen/LanguageScreenUI.kt
@@ -29,7 +29,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -184,7 +183,7 @@ fun LanguageListScreen(
     viewModel: OnboardingViewModel
 ) {
 
-    var selectedLanguage by remember { mutableStateOf<LanguageModel>(viewModel.selectedLanguage) }
+    var selectedLanguage by remember { mutableStateOf(viewModel.selectedLanguage) }
 
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
