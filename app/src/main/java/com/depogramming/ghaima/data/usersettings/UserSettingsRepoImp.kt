419cffb2694206a9565dedcb7f11743ca5c0dc1c
Eldeep1 <alieldeep111@gmail.com>
2026-03-11 00:15:14 +0200

    refactoring the settings repo

--- app/src/main/java/com/depogramming/ghaima/data/usersettings/UserSettingsRepoImp.kt
+++ app/src/main/java/com/depogramming/ghaima/data/usersettings/UserSettingsRepoImp.kt
@@ -2,16 +2,33 @@ package com.depogramming.ghaima.data.usersettings
 
 import com.depogramming.ghaima.R
 import com.depogramming.ghaima.data.onBoarding.LanguageModel
+import com.depogramming.ghaima.data.usersettings.datasource.local.UserSettingsLocalDataSource
+import com.depogramming.ghaima.data.usersettings.model.UserSettingsModel
+import com.depogramming.ghaima.data.utils.toEntity
+import com.depogramming.ghaima.data.utils.toModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class UserSettingsRepoImp :UserSettingsRepo{
+class UserSettingsRepoImp (private val userSettingsLocalDataSource: UserSettingsLocalDataSource):UserSettingsRepo{
+
     override suspend fun getLanguages(): List<LanguageModel> {
 
         val languages = listOf(
-            LanguageModel("English", "United Kingdom", R.drawable.ukflag),
-            LanguageModel("Arabic", "Egypt", R.drawable.egflag)
+            LanguageModel("English", "United Kingdom", R.drawable.ukflag,"en"),
+            LanguageModel("العربية", "مصر", R.drawable.egflag,"ar")
         )
 
         return languages
+    }
+
+    override fun getUserData(): Flow<UserSettingsModel?> {
+        return userSettingsLocalDataSource.getUserData().map { entity ->
+            entity?.toModel()
+        }
+    }
+
+    override suspend fun setUserSettings(userSettingsModel: UserSettingsModel){
+        userSettingsLocalDataSource.setUserData(userSettingsModel.toEntity())
     }
 
 }
