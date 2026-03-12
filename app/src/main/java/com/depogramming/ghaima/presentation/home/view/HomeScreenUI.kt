facc51f6cf4c9e06d5406b3d589782ad5f8927d5
Eldeep1 <alieldeep111@gmail.com>
2026-03-12 23:06:45 +0200

    adding home page appbar

--- app/src/main/java/com/depogramming/ghaima/presentation/home/view/HomeScreenUI.kt
+++ app/src/main/java/com/depogramming/ghaima/presentation/home/view/HomeScreenUI.kt
@@ -1,22 +1,139 @@
 package com.depogramming.ghaima.presentation.home.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.depogramming.ghaima.R
 
 @Composable
-fun HomeScreenUI(modifier: Modifier=Modifier){
+fun HomeScreenUI(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
 
     ) {
-        Text("interestingggg")
+        HomeScreenContent()
     }
+}
+
+@Composable
+fun HomeScreenContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(horizontal = 24.dp)
+    ) {
+        CustomAppBar()
+        TodayWeather()
+        HourlyForeCast()
+        WeekForeCast()
+    }
+}
+
+@Composable
+fun HomeScreenLoading(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun HomeScreenError(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun CustomAppBar(modifier: Modifier = Modifier) {
+    Spacer(Modifier.height(48.dp))
+    Row(modifier = modifier) {
+        Text(
+            text = stringResource(R.string.ghaima),
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Spacer(Modifier.weight(1f))
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .width(152.dp)
+                .height(38.dp)
+                .clip(RoundedCornerShape(24.dp))
+                .background(
+                    Brush.linearGradient(
+                        colors = listOf(
+                            Color.White.copy(alpha = 0.3f),
+                            Color.White.copy(alpha = 0.1f)
+                        )
+                    )
+                )
+                .border(
+                    width = 1.dp,
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color.White.copy(alpha = 0.6f),
+                            Color.Transparent,
+                            Color.White.copy(alpha = 0.2f)
+                        )
+                    ),
+                    shape = RoundedCornerShape(24.dp),
+
+                ),
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 12.dp),
+            ) {
+
+                Image(
+                    painter = painterResource(R.drawable.location_ic),
+                    contentDescription = null
+                )
+                Spacer(Modifier.width(6.dp))
+                Text(
+                    text = "value from view model",
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TodayWeather() {
+
+}
+
+@Composable
+fun HourlyForeCast() {
+
+}
+
+@Composable
+fun WeekForeCast() {
+
 }
