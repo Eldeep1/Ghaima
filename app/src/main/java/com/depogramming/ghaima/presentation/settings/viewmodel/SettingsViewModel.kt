47f774648179137a84bfdd8ea743cb905e789451
Eldeep1 <alieldeep111@gmail.com>
2026-03-14 00:11:39 +0200

    creating settings viewmodel and adding language support

---
+++ app/src/main/java/com/depogramming/ghaima/presentation/settings/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,98 @@
+package com.depogramming.ghaima.presentation.settings.viewmodel
+
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.depogramming.ghaima.data.usersettings.UserSettingsRepo
+import com.depogramming.ghaima.data.usersettings.model.UserSettingsModel
+import com.depogramming.ghaima.presentation.utils.Languages
+import com.depogramming.ghaima.presentation.utils.TemperatureUnit
+import com.depogramming.ghaima.presentation.utils.WindSpeedUnit
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+
+class SettingsViewModel(
+    private val userSettingsRepo: UserSettingsRepo
+) : ViewModel() {
+    //the temperature drop down
+    private val _selectedTempUnitIndex = MutableStateFlow(0)
+    val selectedTempUnitIndex = _selectedTempUnitIndex.asStateFlow()
+    val availableTempUnits = TemperatureUnit.entries
+
+    fun saveTempSelection(selectedIndex: Int) {
+        _selectedTempUnitIndex.value = selectedIndex
+        val value = availableTempUnits[selectedIndex].apiValue
+
+        viewModelScope.launch {
+            updateSettings { it.copy(units = value) }
+        }
+    }
+
+    //the wind drop down
+    private val _selectedWindSpeedIndex = MutableStateFlow(0)
+    val selectedWindSpeedIndex = _selectedWindSpeedIndex.asStateFlow()
+    val availableWindSpeeds = WindSpeedUnit.entries
+
+    fun saveWindSpeedSelection(selectedIndex: Int) {
+        _selectedWindSpeedIndex.value = selectedIndex
+        val dbValueToSave = availableWindSpeeds[selectedIndex].dbValue
+
+        viewModelScope.launch { updateSettings { it.copy(windSpeedUnit = dbValueToSave) } }
+    }
+
+    //the language dropdown
+    private val _selectedLanguageIndex = MutableStateFlow(0)
+    val selectedLanguageIndex = _selectedLanguageIndex.asStateFlow()
+    val languagesList = Languages.entries
+    fun saveLanguageSelection(selectedIndex: Int) {
+        _selectedLanguageIndex.value = selectedIndex
+
+        val languageCode = if (selectedIndex == 0) "en" else "ar"
+
+        val localeList = LocaleListCompat.forLanguageTags(languageCode)
+        AppCompatDelegate.setApplicationLocales(localeList)
+
+        viewModelScope.launch {
+            updateSettings { it.copy(languageCode = languageCode) }
+        }
+    }
+
+    private suspend fun updateSettings(update: (UserSettingsModel) -> UserSettingsModel) {
+        val currentSettings = userSettingsRepo.getUserData().firstOrNull()
+            ?: UserSettingsModel(null, null, null, null)
+
+        userSettingsRepo.setUserSettings(update(currentSettings))
+    }
+
+    init {
+        viewModelScope.launch {
+            userSettingsRepo.getUserData().firstOrNull()?.let { savedSettings ->
+
+                val tempIndex =
+                    availableTempUnits.indexOfFirst { it.apiValue == savedSettings.units }
+
+                _selectedTempUnitIndex.value = tempIndex
+
+
+                val windIndex =
+                    availableWindSpeeds.indexOfFirst { it.dbValue == savedSettings.windSpeedUnit }
+                _selectedWindSpeedIndex.value = windIndex
+
+                _selectedLanguageIndex.value = if (savedSettings.languageCode == "ar") 1 else 0
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    class SettingsViewModelFactory(
+        private val userSettingsRepo: UserSettingsRepo
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return SettingsViewModel(userSettingsRepo) as T
+        }
+    }
+}
---
+++ app/src/main/java/com/depogramming/ghaima/presentation/utils/Languages.kt
@@ -0,0 +1,10 @@
+package com.depogramming.ghaima.presentation.utils
+
+import androidx.annotation.StringRes
+import com.depogramming.ghaima.R
+
+enum class Languages(@StringRes val titleResId: Int) {
+    ENGLISH(R.string.english),
+    ARABIC(R.string.arabic),
+
+}
