e1d4fd41eda92fa77aa073b4a414263190e2e6c5
Eldeep1 <alieldeep111@gmail.com>
2026-03-14 00:23:11 +0200

    adding temperature and wind speed settings dropdowns

--- app/src/main/java/com/depogramming/ghaima/presentation/settings/view/SettingsUI.kt
+++ app/src/main/java/com/depogramming/ghaima/presentation/settings/view/SettingsUI.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,15 +22,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.depogramming.ghaima.R
-import com.depogramming.ghaima.presentation.onboarding.viewmodel.OnboardingViewModel
 import com.depogramming.ghaima.presentation.onboarding.views.utils.SettingsSelectionCard
 import com.depogramming.ghaima.presentation.settings.viewmodel.SettingsViewModel
 
 @Composable
 fun SettingsUI(modifier: Modifier=Modifier,viewModel: SettingsViewModel){
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = modifier
-            .fillMaxSize().padding(horizontal = 24.dp)
+            .fillMaxSize().padding(horizontal = 24.dp).verticalScroll(scrollState)
 
     ) {
         Spacer(Modifier.height(48.dp))
@@ -36,9 +39,15 @@ fun SettingsUI(modifier: Modifier=Modifier,viewModel: SettingsViewModel){
         Spacer(Modifier.height(56.dp))
 
         LanguageDropDown(viewModel)
-//        TemperatureDropDown()
-//        WindSpeedDropDown()
-//        SelectLocationButton()
+        Spacer(Modifier.height(8.dp))
+        TemperatureDropDown(viewModel)
+        Spacer(Modifier.height(8.dp))
+        WindSpeedDropDown(viewModel)
+        Spacer(Modifier.height(8.dp))
+
+    //        SelectLocationButton()
+        Spacer(Modifier.height(16.dp))
+
 
     }
 }
@@ -65,3 +74,25 @@ fun LanguageDropDown(viewModel: SettingsViewModel){
     )
 }
 
+@Composable
+fun TemperatureDropDown(viewModel: SettingsViewModel){
+
+    val selectedTemperature by viewModel.selectedTempUnitIndex.collectAsStateWithLifecycle()
+    SettingsSelectionCard(
+        titleResId = R.string.temperature,
+        optionsResIds = viewModel.availableTempUnits.map { it.titleResId  },
+        selectedIndex = selectedTemperature,
+        onOptionSelected = { index -> viewModel.saveTempSelection(index) }
+    )
+}
+@Composable
+fun WindSpeedDropDown(viewModel: SettingsViewModel){
+
+    val selectedWindSpeed by viewModel.selectedWindSpeedIndex.collectAsStateWithLifecycle()
+    SettingsSelectionCard(
+        titleResId = R.string.wind_speed_title,
+        optionsResIds = viewModel.availableWindSpeeds.map { it.titleResId  },
+        selectedIndex = selectedWindSpeed,
+        onOptionSelected = { index -> viewModel.saveWindSpeedSelection(index) }
+    )
+}
